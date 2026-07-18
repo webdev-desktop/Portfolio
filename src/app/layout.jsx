f333269da9ff27ef05c72af9842d6feb0a31820c
Apurv <webdev.desktop01@gmail.com>
2026-07-18 14:08:35 +0530

    Added Google Analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import Header from "@/components/layout/Header";
+import Script from "next/script";
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar.jsx";
 import { Ubuntu, IBM_Plex_Mono } from "next/font/google";
@@ -40,6 +41,14 @@ export default function RootLayout({ children }) {
             __html: JSON.stringify(personSchema),
           }}
         />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID},{page_path: window.location.pathname}');`}
+        </Script>
       </body>
     </html>
   );
