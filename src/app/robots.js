2abf4cbeacf20046b0c9f8a6169e8a1ede6b44de
Apurv <webdev.desktop01@gmail.com>
2026-07-19 22:06:32 +0530

    Add robots.txt

---
+++ src/app/robots.js
@@ -0,0 +1,14 @@
+export const dynamic = "force-static";
+
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+
+    sitemap: "https://webdevzone.in/sitemap.xml",
+
+    host: "https://webdevzone.in",
+  };
+}
